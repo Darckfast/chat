13ecd783d75e9e7db779f98a4194908310de14a7
Costa <victorcosta37@gmail.com>
2020-11-18 09:32:45 -0300

    small change on chat layout

--- src/web/src/app/login/login-form/login-form.component.ts
+++ src/web/src/app/login/login-form/login-form.component.ts
@@ -30,10 +30,20 @@ export class LoginFormComponent implements OnInit {
     return this.loginForm.get('chatId');
   }
 
-  joinChat() {
+  hasNoAuthToken(): boolean {
+    return !JSON.parse(localStorage.getItem('auth')).auth_token ;
+  }
+
+  joinChat(): void {
     console.log(JSON.stringify(this.loginForm.getRawValue()));
 
     if (this.loginForm.valid) {
+      localStorage.setItem('auth', JSON.stringify({
+        username: this.username.value,
+        password: this.password.value,
+        auth_token: 'yes'
+      }));
+
       this.router.navigate(['/room'], {
         queryParams: {
           id: this.chatId.value,
@@ -42,5 +52,10 @@ export class LoginFormComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const localAuth = JSON.parse(localStorage.getItem('auth'));
+
+    this.username.setValue(localAuth.username);
+    this.password.setValue(localAuth.password);    
+  }
 }
